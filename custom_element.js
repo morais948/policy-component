868296f1aa0d1153c28558ddd930920526be2e7e
matheus morais vieira duarte <matheus.morais@novam3.com.br>
2021-12-22 12:07:36 -0300

    ajustes de contexto :wrench:

--- custom_element.js
+++ custom_element.js
@@ -3,19 +3,23 @@ class CustomPolicy extends HTMLElement{
     constructor(){
         super()
         this.shadow = this.attachShadow({mode: 'open'})
-
+        this.raiz = document.createElement('div')
+        
         //text
         this.purecookieTitle = this.getAttribute("purecookie-title") || "Política de Cookies"
         this.purecookieDesc = this.getAttribute("purecookie-desc") || "Usamos cookies para melhorar sua experiência de navegação no site. Ao utilizar nosso site, trataremos os cookies de acordo com a nossa política de Cookies. Para ter mais informações sobre como isso é feito."
         this.purecookieLink = `<a href="${this.getAttribute("purecookie-link") || '#'}">clique aqui.</a>`
         this.purecookieLabelButton = this.getAttribute("purecookie-label-button") || "ok"
+        this.purecookieTimeTransition = this.getAttribute("purecookie-time-transition") || "500ms"
         //colors
         this.purecookieColorFontTitle = this.getAttribute("purecookie-color-font-title") || "#252525"
         this.purecookieColorFontContent = this.getAttribute("purecookie-color-font-content") || "#252525"
         this.purecookieColorBackgroundBtn = this.getAttribute("purecookie-color-background-btn") || "#2ddad0"
         this.purecookieColorBtn = this.getAttribute("purecookie-color-btn") || "#252525"
         this.purecookieBackgroundColorContent = this.getAttribute("purecookie-background-color-content") || "white"
+        
         this.start()
+        this.shadow.appendChild(this.raiz)
     }
 
     start(){
@@ -38,9 +42,11 @@ class CustomPolicy extends HTMLElement{
                 position: fixed;
                 bottom: 30px;
                 right: 30px;
-                display: none;
+                display: block;
                 border-radius: 5px;
                 box-shadow: 2px 2px 5px #888888;
+                animation-name: anim;
+                animation-duration: ${this.purecookieTimeTransition};
             }
 
             .cookieConsentContainer .cookieTitle a {
@@ -92,6 +98,11 @@ class CustomPolicy extends HTMLElement{
                 color: ${this.purecookieColorBackgroundBtn};
             }
 
+            .animate-reverse{
+                animation-name: anim-reverse !important;
+                animation-duration: ${this.purecookieTimeTransition} !important;
+            }
+
             @media (max-width:980px) {
                 .cookieConsentContainer {
                     bottom: 0 !important;
@@ -100,37 +111,32 @@ class CustomPolicy extends HTMLElement{
                 }
             }
 
-            /*animação de paragrafo de cookies(opcional)*/
             @keyframes anim {
                 from {
-                    background-color: #2ddad0;
-                    opacity: 0.5;
+                    opacity: 0;
                 }
                 to {
-                    background-color: white;
                     opacity: 1;
+                }
+            }
+
+            @keyframes anim-reverse {
+                from {
+                    opacity: 1;
+                }
+                to {
+                    opacity: 0;
                 }
             }
         `
         this.shadow.appendChild(style)
-        document.querySelector('head').appendChild(style)
-    }
-
-    pureFadeIn(e, o) {
-        var i = document.getElementById(e);
-        i.style.opacity = 0, i.style.display = o || "block",
-            function e() {
-                var o = parseFloat(i.style.opacity);
-                (o += .08) > 1 || (i.style.opacity = o, requestAnimationFrame(e))
-            }()
     }
 
     pureFadeOut(e) {
-        var o = document.getElementById(e);
-        o.style.opacity = 1,
-            function e() {
-                (o.style.opacity -= .02) < 0 ? o.style.display = "none" : requestAnimationFrame(e)
-            }()
+        var el = document.querySelector('custom-policy').shadow.querySelector(e)
+        el.addEventListener("webkitAnimationEnd", () => el.style.display = 'none')
+        el.addEventListener("animationend", () => el.style.display = 'none')
+        el.classList.add('animate-reverse')
     }
 
     setCookie(e, o, i) {
@@ -155,16 +161,47 @@ class CustomPolicy extends HTMLElement{
         document.cookie = e + "=; Max-Age=-99999999;"
     }
 
+    showPolicy(){       
+        let div = document.createElement('div')
+        div.setAttribute('class', 'cookieConsentContainer')
+        div.setAttribute('id', 'cookieConsentContainer')
+
+        let div2 = document.createElement('div')
+        div2.setAttribute('class', 'cookieTitle')
+        let aTitle = document.createElement('a')
+        aTitle.innerHTML = this.purecookieTitle
+        div2.appendChild(aTitle)
+
+        let div3 = document.createElement('div')
+        div3.setAttribute('class', 'cookieDesc')
+        let p = document.createElement('p')
+        let aLink = document.createElement('a')
+        aLink.innerHTML = this.purecookieLink
+        p.innerHTML = this.purecookieDesc
+        p.appendChild(aLink)
+        div3.appendChild(p)
+
+        let div4 = document.createElement('div')
+        div4.setAttribute('class', 'cookieButton')
+        let a = document.createElement('a')
+        a.setAttribute('id', 'dismiss')
+        a.innerHTML = this.purecookieLabelButton
+        a.addEventListener('click', this.purecookieDismiss.bind(this))
+        div4.appendChild(a)
+
+        div.appendChild(div2)
+        div.appendChild(div3)
+        div.appendChild(div4)
+
+        this.raiz.appendChild(div)
+    }
+
     cookieConsent() {
-        this.getCookie("purecookieDismiss") || (document.querySelector('body').innerHTML +=
-            '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><a>' +
-            this.purecookieTitle + '</a></div><div class="cookieDesc"><p>' + this.purecookieDesc + " " + this.purecookieLink +
-            '</p></div><div class="cookieButton"><a onClick="purecookieDismiss();">' + this.purecookieLabelButton +
-            "</a></div></div>", this.pureFadeIn("cookieConsentContainer"))
+        this.getCookie("purecookieDismiss") || this.showPolicy()
     }
 
     purecookieDismiss() {
-        this.setCookie("purecookieDismiss", "1", 7), this.pureFadeOut("cookieConsentContainer")
+        this.setCookie("purecookieDismiss", "1", 7), this.pureFadeOut("#cookieConsentContainer")
     }
 }
 
